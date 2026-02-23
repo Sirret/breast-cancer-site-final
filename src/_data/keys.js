ee48c18d266fad38f32616fdc80e2f1816a99379
Sirret <103698757+Sirret@users.noreply.github.com>
2026-02-23 18:01:24 +0000

    fixed payment api and pages design

--- src/_data/keys.js
+++ src/_data/keys.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  paypal: process.env.PAYPAL_CLIENT_ID,
-  modempay: process.env.MODEMPAY_PUBLIC_KEY
+	paypal: process.env.PAYPAL_CLIENT_ID, //set to test?
+	modempay: process.env.MODEMPAY_PUBLIC_KEY,
 };
